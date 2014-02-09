e13eb37e871560464fe1560355982213d0a224c7
veepee <vihamala@cs.helsinki.fi>
2014-02-09 21:55:04 +0200

    Admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ class ApplicationController < ActionController::Base
     redirect_to signin_path, notice:'you should be signed in' if current_user.nil?
   end
 
+  def ensure_that_admin
+    redirect_to signin_path, notice:'this feature needs admin privileges' if current_user.nil? or current_user.admin != true
+  end
+
 end
